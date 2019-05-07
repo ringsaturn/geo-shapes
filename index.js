69905f3e8ac885a973e6a038e7b6b3f8de542a4b
Jay LaPorte <jaylaporte@gmail.com>
2019-05-07 12:31:08 -0400

    v2.0.1: fastfastfast

--- index.js
+++ index.js
@@ -152,6 +152,24 @@ function distance(lat_1, lon_1, lat_2, lon_2) {
   ).s12;
 }
 
+// This distance function is _extremely fast_ and accurate assuming you can
+// guarantee that the distance between the two points is less than 475km.
+// https://en.wikipedia.org/wiki/Geographical_distance#Ellipsoidal_Earth_projected_to_a_plane
+function distance_fast(lat_1, lon_1, lat_2, lon_2) {
+  // https://en.wikipedia.org/wiki/Chebyshev_polynomials
+  const cos_0m = 1;
+  const cos_1m = Math.cos((lat_1 + lat_2) * (Math.PI / 360));
+  const cos_2m = 2 * cos_1m * cos_1m - cos_0m;
+  const cos_3m = 2 * cos_1m * cos_2m - cos_1m;
+  const cos_4m = 2 * cos_1m * cos_3m - cos_2m;
+  const cos_5m = 2 * cos_1m * cos_4m - cos_3m;
+
+  const k_lat = 111132.09 * cos_0m - 566.05 * cos_2m + 1.20 * cos_4m;
+  const k_lon = 111415.13 * cos_1m - 94.55 * cos_3m + 0.12 * cos_5m;
+
+  return Math.hypot(k_lat * (lat_1 - lat_2), k_lon * (lon_1 - lon_2));
+}
+
 function distance_any(a, bs) {
   const m = centroid(a);
   let min_dist = Infinity;
@@ -165,9 +183,11 @@ function distance_any(a, bs) {
   return min_dist;
 }
 
+exports.EARTH_RADIUS = 6371008.8;
 exports.centroid = centroid;
 exports.overlaps = overlaps;
 exports.overlaps_any = overlaps_any;
 exports.bearing = bearing;
 exports.distance = distance;
+exports.distance_fast = distance_fast;
 exports.distance_any = distance_any;
